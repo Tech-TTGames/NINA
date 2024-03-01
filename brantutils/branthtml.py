e1af0390aa35b07b05d0c20a2445794fd9d9d0cf
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2024-03-01 22:08:37 +0100

    Handle lethal HTML files.

--- brantutils/branthtml.py
+++ brantutils/branthtml.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 import tomli_w
@@ -15,7 +16,7 @@ def convert_to_toml(html_data):
         A list of dictionaries containing the converted event data in TOML format.
     """
     soup = BeautifulSoup(html_data, 'html.parser')
-    data = soup.find_all("br")  # Find events separated by double <br>
+    data = soup.find_all("strong")  # Find events separated by double <br>
 
     converted_events = []
     template = {
@@ -24,20 +25,20 @@ def convert_to_toml(html_data):
         "max_cycle": -1,
     }
     current_data = template.copy()
-    for pouch in data:
-        if pouch.next_sibling is None or pouch.next_element.name == 'br':
-            converted_events.append(current_data)
-            current_data = template.copy()
-        elif pouch.previous_sibling.name == 'br':
-            continue
-        elif pouch.next_sibling.name == 'strong':
-            text = str(pouch.previous_element[2:])
+    event_running = False
+    for bag_of_soup in data:
+        if "#" in bag_of_soup.string:
+            if event_running:
+                converted_events.append(current_data)
+                current_data = template.copy()
+            event_running = True
+            text = str(bag_of_soup.next_sibling[2:].strip())
             current_data["refstring"] = text[:]
             for matchfound in re.finditer(r'\((.*?)\)', text):
                 if '/' in matchfound.group(1):
                     text = text.replace(matchfound.group(), matchfound.group(1).split('/')[0])
                 else:
-                    plr_id = matchfound.group(1).replace('Player', "").split(":")[0]
+                    plr_id = re.split(r'[+\-!:]', matchfound.group(1).replace('Player', ""))[0]
                     text = text.replace(matchfound.group(), f"$Tribute{plr_id}")
             for matchfound in re.finditer(r'\[(.*?)\]', text):
                 plchldr_type = matchfound.group(1)[:5]
@@ -49,18 +50,49 @@ def convert_to_toml(html_data):
 
             current_data['text'] = text
             # Also try and generate tribute_requirements based on string, but that probably in a second loop.
-        elif pouch.next_sibling.name == 'a':
-            current_data['tribute_changes'] = int(pouch.previous_element) * [{}]
+        elif "Tributes" in bag_of_soup.string:
+            current_data['tribute_changes'] = [{} for a in range(int(bag_of_soup.next_sibling.strip()))]
+        elif "Killed (By)" in bag_of_soup.string:
+            sets = bag_of_soup.next_sibling.string.strip().split('|')
+            for dataset in sets:
+                killed, by = dataset.split(' (')
+                killed = int(killed.strip()[6:]) - 1
+                by = [int(a[6:]) - 1 for a in by.strip()[:-1].split(', ')]
+                current_data['tribute_changes'][killed]['status'] = 1
+                for killer in by:
+                    if killer == -1:
+                        continue
+                    current_killtotal = current_data['tribute_changes'][killer].get('kills', 0) + 1
+                    current_data['tribute_changes'][killer]['kills'] = current_killtotal
 
     return converted_events
 
 
-# Example usage
-with open("events.html") as f:
-    html_data = f.read()
+def process_html_file(filename):
+    """
+    Reads HTML data from a file, converts it to TOML, and saves it to a corresponding TOML file.
 
-toml_data = convert_to_toml(html_data)
-toml_data = {"cycles": {"events": toml_data}}
+    Args:
+        filename: The name of the HTML file to process.
+    """
+    with open(filename) as f:
+        html_data = f.read()
 
-with open("events.toml", "wb") as f:
-    tomli_w.dump(toml_data, f)
+    toml_data = convert_to_toml(html_data)
+    toml_data = {"cycles": {"events": toml_data}}
+
+    # Create the output filename by replacing the extension with .toml
+    output_filename = os.path.splitext(filename)[0] + ".toml"
+
+    with open(output_filename, "wb") as f:
+        tomli_w.dump(toml_data, f)
+
+
+# Get all HTML files in the current directory
+html_files = [f for f in os.listdir() if f.endswith(".html")]
+
+# Process each HTML file
+for filename in html_files:
+    process_html_file(filename)
+
+print("Converted all HTML files to TOML successfully!")
