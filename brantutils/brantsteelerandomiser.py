12b1e6b45a51b3077c4237d7fdbc098ce3b52e4e
Tech. TTGames <50541739+Tech-TTGames@users.noreply.github.com>
2024-03-03 13:49:37 +0100

    MultiRandomize for brantutils

--- brantutils/brantsteelerandomiser.py
+++ brantutils/brantsteelerandomiser.py
@@ -63,50 +63,61 @@ if __name__ == '__main__':
         help='Output a toml file.',
         default=False,
     )
+    parser.add_argument(
+        '-r',
+        '--repeat',
+        type=int,
+        help='Number of times to randomize cast. Conflicts with --output.',
+        default=1,
+    )
     args = parser.parse_args()
     colorama.init()
     intake = pathlib.Path(args.input)
-    if args.output is None:
-        if args.output_toml:
-            ext = '.toml'
-        else:
-            ext = '.txt'
-        output = pathlib.Path(args.input[:-4] + f'_randomized{ext}')
+    for i in range(args.repeat):
+        if args.output is None:
+            if args.output_toml:
+                ext = '.toml'
+            else:
+                ext = '.txt'
+            no = ""
+            if i:
+                no = f"_{i}"
+            output = pathlib.Path(f'{args.input[:-4]}_randomized{no}{ext}')
 
-    else:
-        output = pathlib.Path(args.output)
-    if not intake.exists():
-        print(colorama.Fore.RED + "Input file does not exist!" + colorama.Style.RESET_ALL)
-        print("Exiting...")
-        exit(1)
-    if output.exists():
-        print(colorama.Fore.RED + "Output file already exists!")
-        print("Are you sure you want to overwrite it? (Y/N)" + colorama.Style.RESET_ALL)
-        if input().upper() != 'Y':
+        else:
+            output = pathlib.Path(args.output)
+        if not intake.exists():
+            print(colorama.Fore.RED + "Input file does not exist!" + colorama.Style.RESET_ALL)
             print("Exiting...")
-            exit(0)
-    print("Reading input...")
-    try:
-        sim = Simulation(intake)
-    except Exception as e:
-        print(colorama.Fore.RED + "Error reading input file!" + colorama.Style.RESET_ALL)
-        print(e)
-        print("Exiting...")
-        exit(1)
-    if args.cast:
-        print("Randomizing cast...")
-        random.shuffle(sim.cast)
-    if args.district_colors:
-        print("Assigning district colors...")
-        max_hue = 360
-        increment = max_hue // len(sim.districts)
-        offset = random.randint(0, increment)
-        for i, district in enumerate(sim.districts):
-            rgb = [int(x * 255) for x in colorsys.hsv_to_rgb((i * increment + offset) / 360, 1.0, 1.0)]
-            color = '#%02x%02x%02x' % (rgb[0], rgb[1], rgb[2])
-            district['color'] = color + " 0 0"
-    if args.output_toml:
-        sim.writet(output)
-    else:
-        sim.write(output)
-    print(colorama.Fore.CYAN + "Done! Results written to " + str(output) + ".")
+            exit(1)
+        if output.exists():
+            print(colorama.Fore.RED + "Output file already exists!")
+            print("Are you sure you want to overwrite it? (Y/N)" + colorama.Style.RESET_ALL)
+            if input().upper() != 'Y':
+                print("Exiting...")
+                exit(0)
+        print("Reading input...")
+        try:
+            sim = Simulation(intake)
+        except Exception as e:
+            print(colorama.Fore.RED + "Error reading input file!" + colorama.Style.RESET_ALL)
+            print(e)
+            print("Exiting...")
+            exit(1)
+        if args.cast:
+            print("Randomizing cast...")
+            random.shuffle(sim.cast)
+        if args.district_colors:
+            print("Assigning district colors...")
+            max_hue = 360
+            increment = max_hue // len(sim.districts)
+            offset = random.randint(0, increment)
+            for i, district in enumerate(sim.districts):
+                rgb = [int(x * 255) for x in colorsys.hsv_to_rgb((i * increment + offset) / 360, 1.0, 1.0)]
+                color = '#%02x%02x%02x' % (rgb[0], rgb[1], rgb[2])
+                district['color'] = color + " 0 0"
+        if args.output_toml:
+            sim.writet(output)
+        else:
+            sim.write(output)
+        print(colorama.Fore.CYAN + "Done! Results written to " + str(output) + ".")
